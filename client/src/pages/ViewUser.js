1009bb73b7d8117b913543876e9a3169404d42d9
Ekta Shahani <shahani.ekta@gmail.com>
2017-09-08 00:31:13 -0700

    View user redux refactoring

--- client/src/pages/ViewUser.js
+++ client/src/pages/ViewUser.js
@@ -4,40 +4,32 @@ import { Link } from 'react-router-dom'
 class ViewUser extends Component {
   state = {_id:'', email: '', password:'', firstName:'', lastName:'', age:0};
 
-  componentWillMount() {
-    var token = sessionStorage.getItem('authToken');
-    var userHeader = new Headers();
-    userHeader.append('x-auth', token);
-
-    fetch('/users/me', {
-      method: 'GET',
-      headers: userHeader
-    }).then((res) => {
-      return res.json();
-    }).then((body) => {
-      this.setState({
-        _id: body._id,
-        email: body.email,
-        firstName: body.firstName,
-        lastName: body.lastName,
-        age: body.age
-      });
-    }).catch((e) => {
-      console.log(e);
-    });
-  }
+  // componentWillMount() {
+  //   var token = sessionStorage.getItem('authToken');
+  //   var userHeader = new Headers();
+  //   userHeader.append('x-auth', token);
+  //
+  //   fetch('/users/me', {
+  //     method: 'GET',
+  //     headers: userHeader
+  //   }).then((res) => {
+  //     return res.json();
+  //   }).then((body) => {
+  //     this.setState({
+  //       _id: body._id,
+  //       email: body.email,
+  //       firstName: body.firstName,
+  //       lastName: body.lastName,
+  //       age: body.age
+  //     });
+  //   }).catch((e) => {
+  //     console.log(e);
+  //   });
+  // }
 
   render() {
     return (
-      <div className="viewUser">
-        <p>
-          {this.state.email} <br/>
-          {this.state.firstName} <br/>
-          {this.state.lastName} <br/>
-        {this.state.age} <br/>
-        </p>
-        <Link to="/edituser">Edit User</Link>
-      </div>
+
     );
   }
 }
