4a803f27b83316a7ebd8e91a73a2944360163352
Ekta Shahani <shahani.ekta@gmail.com>
2017-09-23 13:54:28 -0700

    admin action

--- client/src/actions/poolActions.js
+++ client/src/actions/poolActions.js
@@ -22,6 +22,31 @@ export function fetchPools(query) {
   }
 }
 
+export function fetchPoolsAdmin() {
+  var token = sessionStorage.getItem('authToken');
+  var userHeader = new Headers();
+  userHeader.append('x-auth', token);
+
+  return function(dispatch) {
+    dispatch({type:'FETCH_POOLS'});
+    fetch('/pools/admin/list', {
+      method: 'GET',
+      headers: userHeader
+    }).then((res) => {
+      if(res.ok) {
+        return res.json();
+      }
+      else  {
+        return Promise.reject({status: res.status});
+      }
+    }).then((body) => {
+      dispatch({type: 'FETCH_POOLS_FULFILLED', payload: body});
+    }).catch((e) => {
+      dispatch({type: 'FETCH_POOLS_REJECTED', payload: e});
+    });
+  }
+}
+
 export function setInitLocation(location) {
   return { type:'SET_INIT_LOCATION', payload:location };
 }
